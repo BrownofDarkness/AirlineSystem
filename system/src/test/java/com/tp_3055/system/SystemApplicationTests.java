4590b7189b1650953f249fc67f451c2bfea84974
BrownofDarkness <takoubrown@gmail.com>
2023-01-16 11:38:25 +0100

    initialise project

---
+++ system/src/test/java/com/tp_3055/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp_3055.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
